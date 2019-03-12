bff9c0757580ae4b61fc4c0ca149833920bab9cd
j1nma <j1nma@users.noreply.github.com>
2019-03-12 16:52:08 -0300

    CellIndexMethod

--- src/main/java/algorithms/CellIndexMethod.java
+++ src/main/java/algorithms/CellIndexMethod.java
@@ -2,25 +2,125 @@ package algorithms;
 
 import models.Particle;
 
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
 
 public class CellIndexMethod {
 
 	/**
-	 * Particles that identify a cell.
+	 * Cells from 0 to MxM - 1.
+	 * Each one has a list of CellParticles from that cell number.
+	 * A CellParticle contains a Particle and the cell's position.
 	 */
-	private int[] heads;
+	private static List<List<CellParticle>> cells = new ArrayList<>();
+	private static boolean pbc;
+	private static double rc;
+	private static int M;
+	private static int L;
 
-	/**
-	 *
-	 */
-	private int[] list;
+	public static void run(Queue<Particle> particles,
+	                       int boxSide,
+	                       int matrixSize,
+	                       double interactionRadius,
+	                       boolean periodicBoundaryContour) {
 
+		cells = new ArrayList<>();
+		pbc = periodicBoundaryContour;
+		rc = interactionRadius;
+		M = matrixSize;
+		L = boxSide;
 
-	public static void run(List<Particle> particles, int N, int L, int M, int radiusC) {
+		for (int i = 0; i < M * M; i++)
+			cells.add(new ArrayList<>());
 
+		for (Particle p : particles) {
+			// Calculate particle's cell indexes
+			double cellX = Math.floor(p.getPosition().x / (L / M));
+			double cellY = Math.floor(p.getPosition().y / (L / M));
+
+			// Calculate particle's cell number
+			int cellNumber = (int) (cellY * M + cellX);
+
+			// Add particle to that cell with cell position
+			cells.get(cellNumber).add(new CellParticle(p, new Point2D.Double(cellX, cellY)));
+		}
+
+		for (List<CellParticle> cellParticles : cells) {
+			for (CellParticle cp : cellParticles) {
+				double cellX = cp.cellPosition.x;
+				double cellY = cp.cellPosition.y;
+
+				// Check neighbouring cells from inverted up-side down L shape
+				visitNeighbour(cp.particle, cellX, cellY);
+				visitNeighbour(cp.particle, cellX, cellY + 1);
+				visitNeighbour(cp.particle, cellX + 1, cellY + 1);
+				visitNeighbour(cp.particle, cellX + 1, cellY);
+				visitNeighbour(cp.particle, cellX + 1, cellY - 1);
+			}
+		}
 	}
 
-	public static void run(List<List<Particle>> cells) {
+	private static void visitNeighbour(Particle particle, double cellX, double cellY) {
+
+		if (pbc) {
+			// Reset neighbour cell indexes to comply with contour
+			if (cellX >= M) {
+				cellX = 0;
+			}
+
+			if (cellX < 0) { //TODO: == -1?
+				cellX = M - 1;
+			}
+
+			if (cellY >= M) {
+				cellY = 0;
+			}
+
+			if (cellY > 0) { //TODO: == -1?
+				cellY = M - 1;
+			}
+
+		} else {
+			if (cellX >= M || cellX < 0 || cellY >= M || cellY < 0) {
+				return;
+			}
+		}
+
+		int neighbourCellNumber = (int) (cellY * M + cellX);
+
+		List<CellParticle> neighbourCellParticles = cells.get(neighbourCellNumber);
+
+		for (CellParticle neighbourCellParticle : neighbourCellParticles) {
+			Particle neighbourParticle = neighbourCellParticle.particle;
+			if (neighbourParticle.getId() != particle.getId()) {
+
+				double distance;
+
+				if (pbc) {
+					distance = particle.getPeriodicDistanceBetween(neighbourParticle, L);
+				} else {
+					distance = particle.getDistanceBetween(neighbourParticle);
+				}
+
+				if (distance < rc) {
+					// Mutually add both particles as neighbours
+					particle.addNeighbour(neighbourParticle);
+					neighbourParticle.addNeighbour(particle);
+				}
+
+			}
+		}
+	}
+
+	private static class CellParticle {
+		Particle particle;
+		Point2D.Double cellPosition;
+
+		CellParticle(Particle particle, Point2D.Double cellPosition) {
+			this.particle = particle;
+			this.cellPosition = cellPosition;
+		}
 	}
 }
