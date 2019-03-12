62c367c319c367d9b56552eed4670cd751da4fa3
j1nma <j1nma@users.noreply.github.com>
2019-03-12 15:51:36 -0300

    particle distances

--- src/main/java/models/Particle.java
+++ src/main/java/models/Particle.java
@@ -90,4 +90,39 @@ public class Particle {
 	public void setNeighbours(Set<Particle> neighbours) {
 		this.neighbours = neighbours;
 	}
+
+	/**
+	 * To understand this calculation, think about a 8x8 matrix.
+	 * The distance between (0,0) and (0,7) is 7 without periodic boundaries,
+	 * but with it, it should be smaller (1, since they 'touch' each other).
+	 * So this applies too for (0,5) and (0,6), or, when the index is greater than L/2.
+	 * L is the size of the side of the box/matrix.
+	 */
+	public double getPeriodicDistanceBetween(Particle particle, int L) {
+		Point2D.Double particlePosition = particle.getPosition();
+		double dx = Math.abs(this.position.x - particlePosition.x);
+		if (dx > L / 2)
+			dx = L - dx;
+
+		double dy = Math.abs(this.position.y - particlePosition.y);
+		if (dy > L / 2)
+			dy = L - dy;
+
+		return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+	}
+
+	/**
+	 * The distance between points contemplates border-to-border distance.
+	 * That is why the radii are subtracted.
+	 */
+	public double getDistanceBetween(Particle particle) {
+		Point2D.Double particlePosition = particle.getPosition();
+		return Math.sqrt(Math.pow(position.x - particlePosition.x, 2) +
+				Math.pow(position.y - particlePosition.y, 2))
+				- radius - particle.getRadius();
+	}
+
+	public void addNeighbour(Particle neighbour) {
+		this.neighbours.add(neighbour);
+	}
 }
