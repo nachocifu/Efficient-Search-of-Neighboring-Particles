f211401dac0453df2cea8132d88e459d4fcc1b1b
j1nma <j1nma@users.noreply.github.com>
2019-03-12 15:51:48 -0300

    brute force

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,19 @@
+import algorithms.BruteForce;
+import algorithms.CellIndexMethod;
 import com.google.devtools.common.options.OptionsParser;
 import io.Parser;
 import io.SimulationOptions;
+import models.Particle;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+import java.util.Queue;
 
 public class App {
 
 	public static void main(String[] args) {
+		// Parse command line options
 		OptionsParser parser = OptionsParser.newOptionsParser(SimulationOptions.class);
 		parser.parseAndExitUponError(args);
 		SimulationOptions options = parser.getOptions(SimulationOptions.class);
@@ -14,8 +21,43 @@ public class App {
 		if (options.rc < 0 || options.staticFile.isEmpty() || options.dynamicFile.isEmpty()) {
 			printUsage(parser);
 		}
+
+		// Parse static and dynamic files
 		Parser staticAndDynamicParser = new Parser(options.staticFile, options.dynamicFile);
 		staticAndDynamicParser.parse();
+
+		// Run algorithm
+		runAlgorithm(staticAndDynamicParser.getParticles(), options.bf, options.rc, options.M, options.pbc,
+				options.particle, staticAndDynamicParser.getBoxSide());
+	}
+
+	private static void runAlgorithm(Queue<Particle> particles, boolean bf, double rc, int m, boolean pbc, int particle, int L) {
+		long startTime = System.currentTimeMillis();
+
+		List<List<Particle>> cells = new ArrayList<>();
+
+		if (bf) {
+			BruteForce.run(particles, pbc, rc, L);
+		} else {
+			CellIndexMethod.run(cells);
+		}
+
+		long stopTime = System.currentTimeMillis();
+		long elapsedTime = stopTime - startTime;
+
+		if (bf) {
+			System.out.println("Brute Force execution time: " + elapsedTime + "ms");
+		} else {
+			System.out.println("Cell Index Method execution time: " + elapsedTime + "ms");
+		}
+
+		for (Particle p : particles) {
+			System.out.print(p.getId());
+			for (Particle neighbour : p.getNeighbours()) {
+				System.out.print(" " + neighbour.getId());
+			}
+			System.out.print("\n");
+		}
 	}
 
 	private static void printUsage(OptionsParser parser) {
--- src/main/java/algorithms/BruteForce.java
+++ src/main/java/algorithms/BruteForce.java
@@ -1,4 +1,30 @@
 package algorithms;
 
+import models.Particle;
+
+import java.util.Queue;
+
 public class BruteForce {
+
+	public static void run(Queue<Particle> particles, boolean periodicBoundaryContour, double interactionRadius, int L) {
+		for (Particle p1 : particles) {
+			for (Particle p2 : particles) {
+				if (!p1.equals(p2) && !p1.getNeighbours().contains(p2)) {
+
+					double distance;
+
+					if (periodicBoundaryContour) {
+						distance = p1.getPeriodicDistanceBetween(p2, L);
+					} else {
+						distance = p1.getDistanceBetween(p2);
+					}
+
+					if (distance < interactionRadius) {
+						p1.addNeighbour(p2);
+						p2.addNeighbour(p1);
+					}
+				}
+			}
+		}
+	}
 }
