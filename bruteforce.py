d14ea51ff08ccd5616d981db7819333469bcbb69
j1nma <j1nma@users.noreply.github.com>
2019-03-15 07:00:07 -0300

    fixes

--- bruteforce.py
+++ bruteforce.py
@@ -10,30 +10,19 @@ with open('brute_force.csv', 'w') as f:
 	header.append('Brute Force')
 	csv_writer.writerow(header)
 	for x in range(1, 11):
-		print("x ")
-		print(x)
 		averages = [x]
-		print("averages ")
-		print(averages)
 		std = ['std']
 		for y in range(1, 15):
-			print("y ")
-			print(y)
 			values = []
 			brute_force = 'true' if y == 14 else 'false'
 			for z in range(1, 5):
 				command = 'java -jar ./target/tp1-1.0-SNAPSHOT.jar --staticFile=random/Static-{index}.txt --dynamicFile=random/Dynamic-{index}.txt --radius=1.0 --matrix={matrix} '.format(
 					index=x,
-					matrix=y,
-					brute_force=brute_force
+					matrix=y
 					)
 				if brute_force == 'true':
 					print("Running BF ... ")
-					command = 'java -jar ./target/tp1-1.0-SNAPSHOT.jar --staticFile=random/Static-{index}.txt --dynamicFile=random/Dynamic-{index}.txt --radius=1.0 --bf=true '.format(
-						index=x,
-						matrix=y,
-						brute_force=brute_force
-						)
+					command = 'java -jar ./target/tp1-1.0-SNAPSHOT.jar --staticFile=random/Static-{index}.txt --dynamicFile=random/Dynamic-{index}.txt --radius=1.0 --bf=true '.format(index=x)
 				print(command)
 				p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 				number = None;
@@ -43,7 +32,7 @@ with open('brute_force.csv', 'w') as f:
 					number = number[-1]
 					number = number.replace('ms\n', '')
 					break;
-				values.append(int(float(number)))
+				values.append(int(number))
 			retval = p.wait()
 			averages.append(numpy.mean(values))
 			std.append(numpy.std(values))
